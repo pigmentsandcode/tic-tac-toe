f8423b5421ebd6b4149a44fa16cf3994249a4d5f
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-04-09 17:35:01 -0500

    Add basic UI functionality

--- scripts.js
+++ scripts.js
@@ -1,17 +1,16 @@
 function createGameboard() {
   const boardArr = new Array(9);
-  boardArr.fill("*");
+  boardArr.fill("");
 
   function getBoard() {
     return boardArr;
   }
 
   function markBox(spot, marker) {
-    if (boardArr[spot] === "*") {
+    if (boardArr[spot] === "") {
       boardArr[spot] = marker;
       return true;
     } else {
-      console.log("Spot taken");
       return false;
     }
   }
@@ -48,12 +47,8 @@ const gameController = (function (player1Name, player2Name) {
     return players[activePlayerIndex].getPlayer();
   }
 
-  function printBoard() {
-    const board = gameBoard.getBoard();
-    const seperator = "---------";
-    console.log(
-      `${board[0]} | ${board[1]} | ${board[2]} \n ${seperator} \n${board[3]} | ${board[4]} | ${board[5]} \n ${seperator} \n${board[6]} | ${board[7]} | ${board[8]}`
-    );
+  function getBoard() {
+    return gameBoard.getBoard();
   }
 
   function checkWinner() {
@@ -73,44 +68,79 @@ const gameController = (function (player1Name, player2Name) {
       let combo = winCombos[i];
       if (
         board[combo[0]] === board[combo[1]] &&
-        board[combo[1]] === board[combo[2]]
+        board[combo[1]] === board[combo[2]] &&
+        board[combo[0]] !== ""
       ) {
         winner = board[combo[0]];
         break;
       }
     }
 
-    console.log("Winner: " + winner);
-    if (winner !== "none") return;
+    if (winner !== "none") {
+      winner =
+        winner === "X"
+          ? players[0].getPlayer().name
+          : players[1].getPlayer().name;
+      return;
+    }
 
-    if (!board.includes("*")) {
+    if (!board.includes("")) {
       winner = "tie";
     }
-    console.log("Winner: " + winner);
   }
 
   function playerTurn(spot) {
     const markResult = gameBoard.markBox(spot, getActivePlayer().marker);
+    displayController.renderBoard();
+    checkWinner();
+    if (winner !== "none") {
+      if (winner === "tie") {
+        displayController.renderMessage("Game ends in a tie!");
+      } else {
+        displayController.renderMessage(`${winner} won the game!`);
+      }
+    } else {
+      switchActivePlayer();
+      displayController.renderMessage(
+        `${getActivePlayer().name} (${getActivePlayer().marker}) turn!`
+      );
+    }
   }
 
   return {
-    switchActivePlayer,
     getActivePlayer,
-    printBoard,
+    getBoard,
     playerTurn,
-    checkWinner,
   };
 })("P1", "P2");
 
-gameController.playerTurn(0);
-gameController.playerTurn(2);
-gameController.playerTurn(3);
-gameController.playerTurn(5);
-gameController.playerTurn(7);
-gameController.switchActivePlayer();
-gameController.playerTurn(1);
-gameController.playerTurn(4);
-gameController.playerTurn(6);
-gameController.playerTurn(8);
-gameController.printBoard();
-gameController.checkWinner();
+const displayController = (function () {
+  const boardBtnEls = document.querySelectorAll(".board-btn");
+  const messageEl = document.querySelector(".message");
+
+  function handleSpotClick(e) {
+    const spot = parseInt(e.target.dataset.id);
+    gameController.playerTurn(spot);
+    e.target.removeEventListener("click", handleSpotClick);
+  }
+
+  function renderBoard() {
+    const currBoard = gameController.getBoard();
+    for (let i = 0; i < currBoard.length; i++) {
+      if (currBoard[i] !== "") {
+        const markBtnEl = document.querySelector(`[data-id="${i}"]`);
+        markBtnEl.textContent = currBoard[i];
+      }
+    }
+  }
+
+  function renderMessage(message) {
+    messageEl.textContent = message;
+  }
+
+  boardBtnEls.forEach((btnEl) => {
+    btnEl.addEventListener("click", handleSpotClick);
+  });
+
+  return { renderBoard, renderMessage };
+})();
